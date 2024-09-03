9bdb7c2e9167e451e820cddb5b69951d084e69fe
Hyacinto <jz.juhasz@gmail.com>
2024-09-03 11:25:57 +0200

    LogIn class

--- reptileRiddles2/src/main/java/com/codecool/Main.java
+++ reptileRiddles2/src/main/java/com/codecool/Main.java
@@ -1,7 +1,22 @@
 package com.codecool;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.logging.LocalLogs;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://localhost:3000");
+
+        Dotenv dotenv = Dotenv.load();
+
+        String username = dotenv.get("USER_ID");
+        String password = dotenv.get("PASSWORD");
+        
+        LogIn log = new LogIn();
+        log.logIn(driver, username,password);
     }
 }
