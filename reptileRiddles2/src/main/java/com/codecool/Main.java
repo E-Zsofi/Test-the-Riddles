904c025a2facc3cb9552a8823b598146c674d779
Zsofia Eszter <57215326+E-Zsofi@users.noreply.github.com>
2024-09-03 13:20:13 +0200

    automatic sign up with given credentials in the .env works, the user gets redirected to the login page after creating a new profile

--- reptileRiddles2/src/main/java/com/codecool/Main.java
+++ reptileRiddles2/src/main/java/com/codecool/Main.java
@@ -1,7 +1,33 @@
 package com.codecool;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\csacs\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("acceptInsecureCerts", true);
+        options.addArguments("--disable-search-engine-choice-screen");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:3000/");
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory("src/main/resources")
+                .load();
+
+        String username = dotenv.get("USER1NAME");
+        String password = dotenv.get("PASSWORD");
+        String email = dotenv.get("EMAIL");
+
+        SignUp.Signup(driver, username, password, email);
+
     }
 }
