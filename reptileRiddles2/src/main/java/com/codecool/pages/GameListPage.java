030c6fa67545ac67d795704db02ad8462d143e8d
Zsofia Eszter <57215326+E-Zsofi@users.noreply.github.com>
2024-09-19 10:21:27 +0200

    gamelistPage created with pagefactory, test is in progress

--- reptileRiddles2/src/main/java/com/codecool/pages/GameListPage.java
+++ reptileRiddles2/src/main/java/com/codecool/pages/GameListPage.java
@@ -1,4 +1,31 @@
 package com.codecool.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class GameListPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(css = "#root > div > div.w-full.h-fit.bg-inherit > nav > div > div:nth-child(2) > ul > li:nth-child(2) > a > span")
+    private WebElement quizzesButton;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div[2]/div/div[1]/div[1]/button[2]")
+    private WebElement playButton;
+
+
+    public GameListPage(WebDriver driver, WebDriverWait wait) {
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void showGameList() {
+        quizzesButton.click();
+        playButton.click();
+    }
+
+
 }
---
+++ reptileRiddles2/src/test/java/com/codecool/GameListPageTest.java
@@ -0,0 +1,49 @@
+package com.codecool;
+
+import com.codecool.pages.LoginPage;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+class GameListPageTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private LoginPage loginPage;
+    private String username;
+    private String password;
+    private String BASE_URL;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("acceptInsecureCerts", true);
+        options.addArguments("--disable-search-engine-choice-screen");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        loginPage = new LoginPage(driver, wait);
+        Dotenv dotenv = Dotenv.configure()
+                .directory("src/main/resources")
+                .load();
+        username = dotenv.get("PLAYER_USERNAME");
+        password = dotenv.get("PLAYER_PASSWORD");
+        BASE_URL = dotenv.get("BASE_URL");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    void showGameList() {
+    }
+}
